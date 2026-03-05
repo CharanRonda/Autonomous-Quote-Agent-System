cb7b9021eb1a934961ec3bdfa8b6c5194d21dfd4
CharanRonda <lronda@gitam.in>
2026-03-06 01:10:19 +0530

    Update app to run on specified port from environment

--- app.py
+++ app.py
@@ -27,5 +27,8 @@ def predict():
                            advice=advice,
                            decision=decision)
 
-if __name__=="__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
